d01e206cc095cb29ff1c65db1fa0f3239b0b35bc
Xoorgh <viktorfasth87@gmail.com>
2023-12-04 00:09:20 +0100

    Implment moving circle

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,27 @@ use macroquad::prelude::*;
 
 #[macroquad::main("wrath")]
 async fn main() {
+    let mut x = screen_width() / 2.0;
+    let mut y = screen_height() / 2.0;
+
     loop {
-        clear_background(DARKPURPLE);
+        clear_background(BLUE);
+
+        if is_key_down(KeyCode::Right) {
+            x += 2.0;
+        }
+        if is_key_down(KeyCode::Left) {
+            x -= 2.0;
+        }
+        if is_key_down(KeyCode::Down) {
+            y += 2.0;
+        }
+        if is_key_down(KeyCode::Up) {
+            y -= 2.0;
+        }
+
+        draw_circle(x, y, 16.0, YELLOW);
+
         next_frame().await
     }
 }
